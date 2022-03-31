a2034af3f53257c7701609e388d4fe5fe8b2b863
poonambudde <97157799+poonambudde@users.noreply.github.com>
2022-03-31 13:48:41 +0530

    [Poonam] Added: Create List for products

--- Product Review Management Using LINQ/ProductReview.cs
+++ Product Review Management Using LINQ/ProductReview.cs
@@ -1,10 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Product_Review_Management_Using_LINQ
 {
-    class ProductReview
+    public class ProductReview
     {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
     }
 }
