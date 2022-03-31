60d02590016850239108809550fe5415d00574f0
poonambudde <buddepoonam123@gmail.com>
2022-03-31 16:38:29 +0530

    [Poonam] Added: Skip top 5 records from list & display other records

--- Product Review Management Using LINQ/Management.cs
+++ Product Review Management Using LINQ/Management.cs
@@ -49,14 +49,26 @@ namespace Product_Review_Management_Using_LINQ
         }
 
         // UC5- Retrieves the productId and review.
-        public void RetrieveProductIdAndReview(List<ProductReview> listProductReviews)
+        public void RetrieveProductIdAndReview(List<ProductReview> listProductReview)
         {
             //lambda syntax
-            var recordData = listProductReviews.Select(x => new { ProductId = x.ProductId, Review = x.Review });
+            var recordedData = listProductReview.Select(x => new { ProductId = x.ProductId, Review = x.Review });
             Console.WriteLine("\nProduct id and review = ");
-            foreach (var list in recordData)
+            foreach (var list in recordedData)
             {
                 Console.WriteLine(list.ProductId + "------" + list.Review);
+            }
+        }
+
+        // UC6- Skips the top five records.
+        public void SkipTopFiveRecords(List<ProductReview> listProductReview)
+        {
+            var recordedData = (from productReview in listProductReview
+                                select productReview).Skip(5);
+            Console.WriteLine("\nTop 5 records from list = ");
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductId:-" + list.ProductId + " UserId:-" + list.UserId + " Ratings:-" + list.Rating + " Review:-" + list.Review + " IsLike:-" + list.isLike);
             }
         }
     }
